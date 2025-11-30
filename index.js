b921a218df87b90077f8c234abb1b60e7aaafa5b
OneSummerDay <artem.kraievyi@gmail.com>
2025-11-30 21:01:05 +0200

    feat: add name query parameter handling

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app = express();
 
 
 app.get('/hello/:name', (request, response) => {
-    response.send(`<h1>Hello ${request.params.name}</h1>`)
+    response.send(`<h1>Hello ${request.params.name}!</h1>`)
 });
 
 app.get('/goodbye', (request, response) => {
-    response.send('<h1>Goodbye express!</h1>')
+    response.send(`<h1>Goodbye ${request.query.name}!</h1>`)
 });
 
 app.listen(80);
